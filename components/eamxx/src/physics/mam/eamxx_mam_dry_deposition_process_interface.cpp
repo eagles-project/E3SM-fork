c1c60db8a00523f4f6d094ef5f2c1310b44d59c1
Balwinder Singh <balwindersingh@gmail.com>
2025-02-16 09:04:44 -0800

    Enables cld borne aerosols advection
    Adds fix for rgss=0 bug
    
    Sync machine files with E3SM master
    
    Adds frontier cmake file
    
    Rebases MAM4xx submodule with layer thickness fix
    
    Fixes dry deposition of gases removal from constituent flux

--- components/eamxx/src/physics/mam/eamxx_mam_dry_deposition_process_interface.cpp
+++ components/eamxx/src/physics/mam/eamxx_mam_dry_deposition_process_interface.cpp
@@ -172,13 +172,13 @@ void MAMDryDep::set_grids(
   for(int m = 0; m < num_aero_modes; ++m) {
     const char *cld_nmr_field_name = mam_coupling::cld_aero_nmr_field_name(m);
 
-    add_field<Updated>(cld_nmr_field_name, scalar3d_mid, n_unit, grid_name);
+    add_tracer<Updated>(cld_nmr_field_name, grid_, n_unit);
     for(int a = 0; a < mam_coupling::num_aero_species(); ++a) {
       const char *cld_mmr_field_name =
           mam_coupling::cld_aero_mmr_field_name(m, a);
 
       if(strlen(cld_mmr_field_name) > 0) {
-        add_field<Updated>(cld_mmr_field_name, scalar3d_mid, q_unit, grid_name);
+        add_tracer<Updated>(cld_mmr_field_name, grid_, q_unit);
       }
     }
   }
